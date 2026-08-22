8f5f923c8d4feb6c247e8a48eb95bf075ea113df
yuvakratnaparkhi <yuvakratnaparkhi@gmail.com>
2026-08-22 13:07:06 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        // MinStack minStack = new MinStack();
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int poppedValue = stack.pop();
+        if(poppedValue == minStack.peek()){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
